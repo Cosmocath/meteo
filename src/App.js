b9cf1800b3eeba2cc197609dd5e9a7e32618dc71
Cosmocath <catherine.hermary@gmail.com>
2021-09-14 00:25:13 +0200

    Dockerfile

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ class App extends React.Component {
     const country= e.target.elements.country.value;
 
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?=${city}${country},uk&appid=${API_key}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?=${city},${country}&appid=${API_key}`);
 
     const response = await api_call.json();
     console.log(response);
--- src/app_component/form.component.jsx
+++ src/app_component/form.component.jsx
@@ -6,10 +6,10 @@ const Form = props => {
             <form onSubmit={props.loadWeather}>
                 <div className="row">
                     <div className="col-md-3 offset-md-2">
-                        <input type="text" className="form-control" name="city" placeholder="Ville" autoComplete="off" />
+                        <input type="text" className="form-control" name="city" placeholder="Ville" autoComplete="on" />
                     </div>
                     <div className="col-md-3">
-                        <input type="text" className="form-control" name="country" autoComplete="off" placeholder="Pays" />
+                        <input type="text" className="form-control" name="country" autoComplete="on" placeholder="Pays" />
                     </div>
                     <div className="col-md-3 mt-md-0 text-md-left">
                         <button className="btn btn-warning">Météo !!!</button>
